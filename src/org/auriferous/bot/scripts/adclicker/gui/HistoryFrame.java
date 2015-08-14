e4f13c1421e6203d71a7a66e391251afb6dc61d9
Jacob Shirley <auriferous95@gmail.com>
2015-08-14 23:02:50 +0100

    Updated jxbrowser to 4.2 and added an agent.

--- src/org/auriferous/bot/scripts/adclicker/gui/HistoryFrame.java
+++ src/org/auriferous/bot/scripts/adclicker/gui/HistoryFrame.java
@@ -3,5 +3,10 @@ package org.auriferous.bot.scripts.adclicker.gui;
 import javax.swing.JFrame;
 
 public class HistoryFrame extends JFrame{
-
+	public HistoryFrame() {
+		super("History");
+		
+		setSize(400, 500);
+		setVisible(true);
+	}
 }
